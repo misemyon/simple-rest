3a8d133b328fca8cc15e5fbdc980b8494d680057
Mischenko <mischenko@ipebi.ru>
2019-12-03 14:04:02 +0500

    add player

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/api/players')
 def get_players():
-    return jsonify([Player('Mike', 205).json(), Player('Rocky', 195).json()])
+    return jsonify([Player('Mike', 205).json(), Player('Rocky', 195).json(), Player('Bob', 193).json()])
 
 
 if __name__ == '__main__':
